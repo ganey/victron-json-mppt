7e84c6c5978ae789fbde5fc70b341a5b1356bd59
Silios <marios.tsalkidis@gmail.com>
2023-07-19 17:15:25 +0300

    Update dbus-service.py
    Updated broken velib references

--- dbus-service.py
+++ dbus-service.py
@@ -14,20 +14,28 @@ import sys  # system command library
 from dbus.mainloop.glib import DBusGMainLoop
 
 # importing dbus complements
-sys.path.insert(1, os.path.join(os.path.dirname(__file__), './ext/velib_python'))
+# Victron packages
+sys.path.insert(
+    1,
+    os.path.join(
+        os.path.dirname(__file__),
+        "/opt/victronenergy/dbus-systemcalc-py/ext/velib_python",
+    ),
+)
 from vedbus import VeDbusService  # VeDbusItemImportObject paths that are mandatory for services representing products
 
-import gobject
-from gobject import idle_add
+from gi.repository import GObject as gobject
+#import gobject
+#from gobject import idle_add
 
 DBusGMainLoop(set_as_default=True)
-dbusservice = VeDbusService('com.victronenergy.solarcharger.epever_ttyUSB1')
+dbusservice = VeDbusService('com.victronenergy.solarcharger.epever_ttyUSB3')
 #
 # mainloop = gobject.MainLoop()
 # mainloop.run()
 
 try:
-    print "trying something"
+    print ("trying something")
     dbusservice.add_path('/Management/ProcessName', __file__)
     dbusservice.add_path('/Management/ProcessVersion',
                          'Version {} running on Python {}'.format(1, sys.version))
@@ -36,14 +44,14 @@ try:
     # Create the mandatory objects
     dbusservice.add_path('/DeviceInstance', 291)
     dbusservice.add_path('/ProductId', 0)
-    dbusservice.add_path('/ProductName', 'JSON MPPT')
+    dbusservice.add_path('/ProductName', 'Epever Tracer 3210N XDS')
     dbusservice.add_path('/FirmwareVersion', 1)
     dbusservice.add_path('/HardwareVersion', 1)
     dbusservice.add_path('/Connected', 1)
     dbusservice.add_path('/Serial', '01234abcde')
 
 except:
-    print "Json mppt has been created before"
+    print ("Json mppt schema has been created before!")
 
 try:
     dbusservice.add_path('/State', 0, writeable=True)
@@ -61,10 +69,10 @@ try:
     dbusservice.add_path('/Mode', 0, writeable=True)
     dbusservice.add_path('/MppOperationMode', 0, writeable=True)
 except:
-    print "Json mppt values created before"
+    print ("Json mppt values created before!2")
 
 
-print "Starting main loop..."
+print ("Starting main loop...")
 mainloop = gobject.MainLoop()
 mainloop.run()
 
